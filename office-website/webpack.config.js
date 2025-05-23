8ad5c07920b84cd6c794f9fa90ea14ba712dde5d
CC11001100 <CC11001100@qq.com>
2025-05-24 01:38:51 +0800

    fix: 移除HtmlWebpackPlugin的favicon配置，避免与HTML中的配置冲突

--- office-website/webpack.config.js
+++ office-website/webpack.config.js
@@ -67,7 +67,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
-      favicon: './public/logo.png',
       inject: true,
       publicPath: baseUrl
     }),
